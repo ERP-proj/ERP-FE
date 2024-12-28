04d71f5a78bce81e12b86549e91c623f6dffabe4
임성경 <5622lsk@naver.com>
2024-12-29 03:28:19 +0900

    style: memberRow 스타일 수정 (#9)

--- src/app/components/member/MemberRow.tsx
+++ src/app/components/member/MemberRow.tsx
@@ -1,9 +1,9 @@
 export default function MemberRow({ member }: { member: any }) {
   return (
-    <div className="flex items-center justify-between p-4 bg-[#F2F8ED] rounded-lg ">
+    <div className="flex flex-wrap items-center justify-between p-4 bg-[#F2F8ED] rounded-lg gap-4">
       {/* 프로필 사진 및 기본 정보 */}
-      <div className="flex items-center gap-4 flex-1">
-        <div className="w-24 h-24 bg-[#fff] rounded-full flex justify-center items-center text-white text-lg">
+      <div className="flex items-center gap-4 flex-1 min-w-[250px]">
+        <div className="w-16 h-16 sm:w-24 sm:h-24 bg-[#fff] rounded-full flex justify-center items-center text-white text-lg">
           {member.photoUrl ? (
             <img
               src={member.photoUrl}
@@ -15,12 +15,12 @@ export default function MemberRow({ member }: { member: any }) {
           )}
         </div>
         <div>
-          <div className="flex items-center gap-2">
-            <div className="text-gray-800 font-bold text-[16px]">
+          <div className="flex items-center gap-2 w-30 flex-wrap">
+            <div className="text-gray-800 font-bold text-sm sm:text-[16px]">
               {member.name}
             </div>
             <div
-              className={`px-2 py-1 text-sm rounded ${
+              className={`px-2 py-1 text-xs sm:text-sm rounded ${
                 member.gender === "남"
                   ? "bg-blue-100 text-blue-600"
                   : "bg-pink-100 text-pink-600"
@@ -28,40 +28,59 @@ export default function MemberRow({ member }: { member: any }) {
             >
               {member.gender}
             </div>
-            <div className="text-sm bg-gray-100 px-2 py-1 rounded text-gray-700">
-              {member.planType}
+            <div
+              className={`px-2 py-1 text-xs sm:text-sm rounded ${
+                member.licenseType === "1종"
+                  ? "bg-white text-gray-500"
+                  : "bg-gray-300 text-gray-800"
+              }`}
+            >
+              {member.licenseType}
             </div>
           </div>
-          <div className="text-gray-600 text-sm">{member.phone}</div>
+          <div className="text-gray-600 text-xs sm:text-sm">{member.phone}</div>
+          <div
+            className={`px-2 w-12 sm:w-14 py-1 justify-center text-xs sm:text-sm rounded-full ${
+              member.planType === "시간제"
+                ? "bg-[#3C6229] text-white"
+                : "bg-[#B4D89C] text-black"
+            }`}
+          >
+            {member.planType}
+          </div>
         </div>
       </div>
 
       {/* 이용권 종류 */}
-      <div className="flex items-center font-[500] justify-center flex-1">
-        <div className="text-sm bg-[#f6f6f6] border border-[#d1d1d1] px-4 py-2 rounded-full text-[#3a3a3a]">
-          이용권 종류
+      <div className="flex items-center font-[500] justify-center flex-1 min-w-[150px]">
+        <div className="text-xs sm:text-sm bg-[#f6f6f6] border border-[#d1d1d1] px-3 sm:px-4 py-1 sm:py-2 rounded-full text-[#3a3a3a]">
+          {member.planName}
         </div>
       </div>
 
       {/* 남은 시간 */}
-      <div className="flex flex-col items-center flex-1">
-        <div className="text-red-600 font-semibold text-lg">
+      <div className="flex flex-col items-center flex-1 min-w-[120px]">
+        <div className="text-red-600 font-semibold text-base sm:text-lg">
           {member.remainingTime}
         </div>
-        <div className="text-sm text-gray-600">{member.usage}</div>
+        <div className="text-xs sm:text-sm text-gray-600">{member.usage}</div>
       </div>
 
       {/* 지각/결석 */}
-      <div className="flex flex-col items-center justify-center bg-white border border-[#B4D89C] rounded-lg w-[86px] h-[70px]">
-        <span className="text-gray-500 text-sm leading-tight">지각/결석</span>
-        <span className="text-gray-800 font-bold text-lg">2/1</span>
+      <div className="flex flex-col items-center justify-center bg-white border border-[#B4D89C] rounded-lg w-[70px] sm:w-[86px] h-[60px] sm:h-[70px]">
+        <span className="text-gray-500 text-xs sm:text-sm leading-tight">
+          지각/결석
+        </span>
+        <span className="text-gray-800 font-bold text-sm sm:text-lg">2/1</span>
       </div>
 
       {/* 등록일 및 기타 결제 */}
-      <div className="flex flex-col items-end flex-1">
-        <div className="text-gray-500 text-sm">{member.registrationDate}</div>
+      <div className="flex flex-col items-end flex-1 min-w-[150px]">
+        <div className="text-gray-500 text-xs sm:text-sm">
+          {member.registrationDate}
+        </div>
         <div
-          className={`text-sm font-bold ${
+          className={`text-xs sm:text-sm font-bold ${
             member.payment > 0 ? "text-green-600" : "text-red-600"
           }`}
         >
