40ce7cdd521ade30660fd9622d22918fb2eb3712
임성경 <5622lsk@naver.com>
2024-12-31 19:22:26 +0900

    feat: 아코디언 하단 수정 (#13)

--- src/app/components/member/CreateMember.tsx
+++ src/app/components/member/CreateMember.tsx
@@ -190,120 +190,149 @@ const CreateMember = () => {
               title="이용권 결제"
               isOpen={accordionStates["이용권결제"]}
               toggleOpen={() => toggleAccordion("이용권결제")}
-            >
-              <div className="bg-white rounded-lg h-[550px] overflow-y-scroll">
-                <h3 className="text-md bg-[#F6F6F6] p-2 m-0 text-[#0D0D0D] font-bold">
-                  이용권 정보
-                </h3>
-
-                {/* 구분 */}
-                <div className="mb-4 p-4">
-                  <h4 className="text-sm font-bold mb-2">구분</h4>
-                  <div className="flex gap-2">
-                    <button className="w-1/2 py-2 rounded-md bg-[#3C6229] text-white text-sm font-semibold">
-                      1종
-                    </button>
-                    <button className="w-1/2 py-2 rounded-md bg-gray-200 text-gray-600 text-sm font-semibold">
-                      2종
-                    </button>
-                  </div>
-                </div>
-
-                {/* 이용권 */}
-                <div className="mb-4 px-4">
-                  <h4 className="text-sm font-bold mb-2">이용권</h4>
-                  <Dropdown
-                    options={[
-                      "10시간 이용권",
-                      "15시간 이용권",
-                      "20시간 이용권",
-                      "1개월 이용권",
-                    ]}
-                    placeholder="이용권 선택"
-                    defaultValue="10시간 이용권"
-                    className="w-full p-2"
-                  />
-                </div>
-
-                {/* 할인 */}
-                <div className="mb-4">
+              children={
+                <div className="bg-white rounded-lg h-[900px] overflow-y-scroll">
                   <h3 className="text-md bg-[#F6F6F6] p-2 m-0 text-[#0D0D0D] font-bold">
-                    할인
+                    이용권 정보
                   </h3>
-                  <h4 className="text-sm font-bold pl-4 pt-4">할인 상품명</h4>
-                  <Dropdown
-                    options={[
-                      "10시간 이용권",
-                      "15시간 이용권",
-                      "20시간 이용권",
-                      "1개월 이용권",
-                    ]}
-                    placeholder="할인 상품 선택"
-                    defaultValue="10시간 이용권"
-                    className="w-full p-4"
-                  />
-                  <div className="flex gap-4 mt-4 px-4">
-                    <div className="w-1/2">
-                      <h4 className="text-sm font-bold mb-2">할인율 (%)</h4>
-                      <input
-                        type="text"
-                        placeholder="할인율 입력"
-                        className="w-full input-content"
-                      />
-                    </div>
-                    <div className="w-1/2">
-                      <h4 className="text-sm font-bold mb-2">
-                        할인율 적용 금액
-                      </h4>
-                      <input
-                        type="text"
-                        placeholder="할인 금액 입력"
-                        className="w-full mb-2 input-content"
-                      />
+
+                  {/* 구분 */}
+                  <div className="mb-4 p-4">
+                    <h4 className="text-sm font-bold mb-2">구분</h4>
+                    <div className="flex gap-2">
+                      <button className="w-1/2 py-2 rounded-md bg-[#3C6229] text-white text-sm font-semibold">
+                        1종
+                      </button>
+                      <button className="w-1/2 py-2 rounded-md bg-gray-200 text-gray-600 text-sm font-semibold">
+                        2종
+                      </button>
                     </div>
                   </div>
-                </div>
 
-                {/* 결제 정보 */}
-                <div className="mb-4">
-                  <h3 className="text-md bg-[#F6F6F6] p-2 m-0 text-[#0D0D0D] font-bold">
-                    결제 정보
-                  </h3>
-                  <div className="p-4">
-                    <h4 className="text-sm font-bold mb-2">결제 방법</h4>
-                    <div className="flex gap-2 py-2">
-                      <button className="flex-1 py-2 rounded-md bg-gray-200 text-gray-600 text-sm font-semibold border border-gray-300">
-                        현금
-                      </button>
-                      <button className="flex-1 py-2 rounded-md bg-gray-200 text-gray-600 text-sm font-semibold border border-gray-300">
-                        카드
-                      </button>
-                      <button className="flex-1 py-2 rounded-md bg-gray-200 text-gray-600 text-sm font-semibold border border-gray-300">
-                        계좌이체
-                      </button>
-                      <button className="flex-1 py-2 rounded-md bg-[#3C6229] text-white text-sm font-semibold border border-gray-300">
-                        기타
-                      </button>
-                    </div>
-                    <input
-                      type="text"
-                      placeholder="기타 입력"
-                      className="p-4 input-content"
+                  {/* 이용권 */}
+                  <div className="mb-4 px-4">
+                    <h4 className="text-sm font-bold mb-2">이용권</h4>
+                    <Dropdown
+                      options={[
+                        "10시간 이용권",
+                        "15시간 이용권",
+                        "20시간 이용권",
+                        "1개월 이용권",
+                      ]}
+                      placeholder="이용권 선택"
+                      defaultValue="10시간 이용권"
+                      className="w-full p-2"
                     />
+                  </div>
 
-                    {/* 등록일 */}
-                    <div className="mb-4">
-                      <h4 className="text-sm font-bold mb-2 pt-4">등록일</h4>
+                  {/* 할인 */}
+                  <div className="mb-4">
+                    <h3 className="text-md bg-[#F6F6F6] p-2 m-0 text-[#0D0D0D] font-bold">
+                      할인
+                    </h3>
+                    <h4 className="text-sm font-bold pl-4 pt-4">할인 상품명</h4>
+                    <Dropdown
+                      options={[
+                        "10시간 이용권",
+                        "15시간 이용권",
+                        "20시간 이용권",
+                        "1개월 이용권",
+                      ]}
+                      placeholder="할인 상품 선택"
+                      defaultValue="10시간 이용권"
+                      className="w-full p-4"
+                    />
+                    <div className="flex gap-4 mt-4 px-4">
+                      <div className="w-1/2">
+                        <h4 className="text-sm font-bold mb-2">할인율 (%)</h4>
+                        <input
+                          type="text"
+                          placeholder="할인율 입력"
+                          className="w-full input-content"
+                        />
+                      </div>
+                      <div className="w-1/2">
+                        <h4 className="text-sm font-bold mb-2">
+                          할인율 적용 금액
+                        </h4>
+                        <input
+                          type="text"
+                          placeholder="할인 금액 입력"
+                          className="w-full mb-2 input-content"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* 결제 정보 */}
+                  <div className="mb-4">
+                    <h3 className="text-md bg-[#F6F6F6] p-2 m-0 text-[#0D0D0D] font-bold">
+                      결제 정보
+                    </h3>
+                    <div className="p-4">
+                      <h4 className="text-sm font-bold mb-2">결제 방법</h4>
+                      <div className="flex gap-2 py-2">
+                        <button className="flex-1 py-2 rounded-md bg-gray-200 text-gray-600 text-sm font-semibold border border-gray-300">
+                          현금
+                        </button>
+                        <button className="flex-1 py-2 rounded-md bg-gray-200 text-gray-600 text-sm font-semibold border border-gray-300">
+                          카드
+                        </button>
+                        <button className="flex-1 py-2 rounded-md bg-gray-200 text-gray-600 text-sm font-semibold border border-gray-300">
+                          계좌이체
+                        </button>
+                        <button className="flex-1 py-2 rounded-md bg-[#3C6229] text-white text-sm font-semibold border border-gray-300">
+                          기타
+                        </button>
+                      </div>
                       <input
-                        type="date"
-                        defaultValue="2024-12-01"
-                        className="w-full p-2 border border-gray-300 rounded-md text-sm bg-[#F2F8ED] text-[#3C6229]"
+                        type="text"
+                        placeholder="기타 입력"
+                        className="p-4 input-content"
                       />
+
+                      {/* 등록일 */}
+                      <div className="mb-4">
+                        <h4 className="text-sm font-bold mb-2 pt-4">등록일</h4>
+                        <input
+                          type="date"
+                          defaultValue="2024-12-01"
+                          className="input-content"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </Accordion>
+              }
+              children2={
+                <div className="flex flex-col w-full">
+                  {/* 총금액 */}
+                  <div className="flex justify-between items-center border-t border-[#dfdfdf] p-4">
+                    <h4 className="text-sm font-bold">총 금액</h4>
+                    <p className="text-2xl font-bold text-red-500">215,000</p>
+                  </div>
+
+                  {/* 하단 버튼 */}
+                  <div className="flex justify-between items-center px-4">
+                    <label className="flex items-center gap-2 text-sm">
+                      <input type="checkbox" className="w-4 h-4" />
+                      미납 여부
+                    </label>
+                    <div className="flex gap-2">
+                      <BasicButton size="small" color="danger" border={false}>
+                        삭제
+                      </BasicButton>
+                      <BasicButton size="small" color="secondary" border={true}>
+                        취소
+                      </BasicButton>
+                      <BasicButton size="small" color="primary" border={false}>
+                        저장
+                      </BasicButton>
+                    </div>
+                  </div>
+                </div>
+              }
+            />
             <Accordion
               title="기타 결제"
               isOpen={accordionStates["기타결제"]}
--- src/app/components/ui/Accordion.tsx
+++ src/app/components/ui/Accordion.tsx
@@ -6,6 +6,7 @@ interface Props {
   isOpen: boolean; // 아코디언 열림 여부
   toggleOpen: () => void; // 아코디언 열림/닫힘 함수
   children?: React.ReactNode;
+  children2?: React.ReactNode;
 }
 
 const Accordion: React.FC<Props> = ({
@@ -13,9 +14,11 @@ const Accordion: React.FC<Props> = ({
   isOpen,
   toggleOpen,
   children,
+  children2,
 }) => {
   return (
-    <div className="m-0">
+    <div className="m-0 relative">
+      {/* 아코디언 버튼 */}
       <button
         className="w-full bg-[#F2F8ED] flex justify-between items-center p-4 text-left text-lg font-bold mb-0.5 text-[#3C6229]"
         onClick={toggleOpen}
@@ -23,7 +26,18 @@ const Accordion: React.FC<Props> = ({
         {title}
         <span>{isOpen ? <SlArrowUp /> : <SlArrowDown />}</span>
       </button>
-      {isOpen && <div className="m-auto">{children}</div>}
+
+      {/* 아코디언 컨텐츠 */}
+      {isOpen && (
+        <div className="relative">
+          <div className="overflow-y-auto h-[570px] px-3 ">{children}</div>
+
+          {/* 하단 그라데이션 고정 박스 */}
+          <div className="absolute bottom-0 left-0 right-0  bg-white h-[120px] p-4 flex justify-between items-center">
+            {children2}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
