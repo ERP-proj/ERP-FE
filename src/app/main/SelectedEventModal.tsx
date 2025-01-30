f88ceb3586f4abd8e3f0945c599c1e8719473169
gimgyuwon <gimgyuwon2@gmail.com>
2025-01-30 09:41:40 -0700

    feat: 모달 버튼 위치 수정

--- src/app/main/SelectedEventModal.tsx
+++ src/app/main/SelectedEventModal.tsx
@@ -358,19 +358,31 @@ const SelectedEventModal: React.FC<EventProps> = ({ event, onClose }) => {
 
       <div className="flex">
         {/* Edit & Save button */}
-        <Button
-          className="flex flex-1 font-light bg-[#D1D1D1] border-0 rounded-lg text-[#FFFFFF] p-2 mt-4 mr-2"
-          onClick={handleSubmit}
-        >
-          {event?.mode === "add" ? "저장" : "수정 완료"}
-        </Button>
+        {event?.mode === "add" && (
+          <div className="flex flex-1">
+            <Button
+              className="flex flex-1 font-light bg-[#D1D1D1] border-0 rounded-lg text-[#FFFFFF] p-2 mt-4 mr-2"
+              onClick={handleSubmit}
+            >
+              저장
+            </Button>
+          </div>
+        )}
         {event?.mode === "edit" && (
-          <Button
-            className="flex flex-1 font-light bg-[#FFFFFF] border-2 border-[#DB5461] rounded-lg text-[#DB5461] p-2 mt-4"
-            onClick={handleDelete}
-          >
-            삭제
-          </Button>
+          <div className="flex flex-1 gap-4">
+            <Button
+              className="flex flex-1 font-light bg-[#FFFFFF] border-2 border-[#DB5461] rounded-lg text-[#DB5461] p-2 mt-4"
+              onClick={handleDelete}
+            >
+              삭제
+            </Button>
+            <Button
+              className="flex flex-1 font-light bg-[#D1D1D1] border-0 rounded-lg text-[#FFFFFF] p-2 mt-4 mr-2"
+              onClick={handleSubmit}
+            >
+              수정 완료
+            </Button>
+          </div>
         )}
       </div>
     </div>
