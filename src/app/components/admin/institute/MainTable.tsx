dfb382b041ed53ceed6d76fa0d033cf068e4ec46
임성경 <5622lsk@naver.com>
2025-02-27 01:22:38 +0900

    fix:빌드에러러fix (#36)

--- src/app/components/admin/institute/MainTable.tsx
+++ src/app/components/admin/institute/MainTable.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Table from "../../ui/Table";
 
-interface Props {}
-
 const storeColumns = [
   { name: "매장명", width: "70%" },
   { name: "영업시간", width: "40%" },
@@ -14,7 +12,7 @@ const storeData = [
   { 매장명: "매장 3", 영업시간: "08:00 ~ 22:00" },
 ];
 
-const MainTable: React.FC<Props> = ({}) => {
+const MainTable: React.FC = ({}) => {
   return (
     <div className="flex w-full max-w-6xl min-h-screen">
       <div className="w-full ">
