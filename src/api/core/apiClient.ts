6ee2594c6637c358578e5cb6335e1aaf0655b335
임성경 <5622lsk@naver.com>
2025-03-07 15:48:48 +0900

    fix: 리프레쉬토큰요청함수 fix (#58)

--- src/api/core/apiClient.ts
+++ src/api/core/apiClient.ts
@@ -36,8 +36,8 @@ apiClient.interceptors.response.use(
       }
       try {
         // Refresh Token을 이용한 새로운 Access Token 요청
-        const refreshResponse = await axios.post(
-          `${process.env.NEXT_PUBLIC_SERVER_URL}/api/account/reissueToken`,
+        const refreshResponse = await apiClient.post(
+          "/api/account/reissueToken",
           null,
           {
             params: { refreshToken },
